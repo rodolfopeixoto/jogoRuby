ddd1f219faa1b90b7301078110ff40fe6c427e45
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-22 19:35:31 -0200

    Added points system

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -64,11 +64,19 @@
   da_boas_vindas
   numero_secreto = sorteia_numero_secreto
 
+  pontos_ate_agora = 1000
+
 limite_de_tentativas = 5
 chutes = []
 
 for tentativa in 1..limite_de_tentativas
   chute          = pede_um_numero chutes, tentativa, limite_de_tentativas
   chutes << chute
+
+  pontos_a_perder  = (chute - numero_secreto) / 2
+  pontos_ate_agora -= pontos_a_perder
+
   break if verifica_se_acertou numero_secreto, chute
 end
+
+puts "Você ganhou #{pontos_ate_agora} pontos."
