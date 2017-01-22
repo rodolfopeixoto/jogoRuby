1d0664dfb20afe0b096a93b4e9a17fff38de8cb9
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-22 14:16:51 -0200

    Refactor puts newline

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,21 +1,17 @@
 puts "Bem vindo ao jogo da advinhação"
 puts "Qual é o seu nome?"
 nome = gets
-puts
-puts
-puts
-puts
+puts "\n\n\n\n"
 puts "Começaremos o jogo para você, " + nome
 
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal advinha hoje o número secreto?"
 
-puts
-puts
-puts
+
+puts "\n\n\n\n"
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
-puts "Será que acertou? Você chutou " + chute
+puts "Será que acertou? Você chutou \n" + chute
 puts chute.to_i == numero_secreto
