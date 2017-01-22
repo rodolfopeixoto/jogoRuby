912754de7ac4b2fdbff6adcbcf05419bbf6fcc5d
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-22 15:34:08 -0200

    Created 'for' for multiple attempts

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,17 +1,52 @@
-puts "Bem vindo ao jogo da advinhação"
+puts "########################################################"
+puts "\tSeja-bem vindo ao jogo da advinhação"
+puts "########################################################"
+puts "\n\n"
+
 puts "Qual é o seu nome?"
 nome = gets
-puts "\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
+puts "\n\n"
 
-puts "Escolhendo um número secreto entre 0 e 200..."
+puts "---------------------------------------------"
+puts "Começaremos o jogo para você, " + nome
+puts "---------------------------------------------"
+puts "\n"
+
+puts "Escolha um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal advinha hoje o número secreto?"
 
+limite_de_tentativas = 5
 
-puts "\n\n\n\n"
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou \n" + chute
-puts chute.to_i == numero_secreto
+for tentativa in 1..limite_de_tentativas
+        puts "\n\n"
+        puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+        puts "Entre com o número"
+        chute = gets
+        puts "Será que acertou? Você chutou \n" + chute
+
+        acertou = numero_secreto == chute.to_i
+
+        if acertou
+            puts "**************************************"
+            puts "Acertou!"
+            puts "**************************************"
+            puts "\n"
+            break
+        else
+
+            maior = numero_secreto > chute.to_i
+
+            if maior
+              puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+              puts "O número secreto é maior!"
+              puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+              puts "\n"
+            else
+              puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+              puts "O número secreto é menor!"
+              puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+              puts "\n"
+            end
+        end
+end
