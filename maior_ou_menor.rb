20d8f0f6cc1ae0a9a0d556d5786f8d276d3a0846
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-22 00:22:37 -0200

    First commit

---
+++ maior_ou_menor.rb
@@ -0,0 +1,20 @@
+puts "Bem vindo ao jogo da advinhação"
+puts "Qual é o seu nome?"
+nome = gets
+puts
+puts
+puts
+puts
+puts "Começaremos o jogo para você, " + nome
+
+puts "Escolhendo um número secreto entre 0 e 200..."
+numero_secreto = 175
+puts "Escolhido... que tal advinha hoje o número secreto?"
+
+puts
+puts
+puts
+puts "Tentativa 1"
+puts "Entre com o número"
+chute = gets
+puts "Será que acertou? Você chutou " + chute
