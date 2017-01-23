eb0cbbef9eb5661951276096eb37598c481d0b49
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-23 18:18:01 -0200

    Refactoring and dividing main file

--- forca.rb
+++ forca.rb
@@ -53,11 +53,15 @@
 
   end
 
-nome = da_boas_vindas
 
-loop do
-  joga nome
-  if nao_quer_jogar?
-    break
+  def jogo_da_forca
+    nome = da_boas_vindas
+
+    loop do
+      joga nome
+      if nao_quer_jogar?
+        break
+      end
+    end
+
   end
-end
