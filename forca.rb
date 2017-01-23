9724121cdf7d268a985e506a953de79fd5c2ed62
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-23 18:06:44 -0200

    Refactoring and dividing business model logic

--- forca.rb
+++ forca.rb
@@ -1,33 +1,4 @@
-  def da_boas_vindas
-    puts "Bem vindo ao jogo da forca"
-    puts "Qual é o seu nome?"
-    nome = gets.strip
-    puts "\n\n\n"
-    puts "Começaremos o jogo para você, #{nome}."
-  end
-
-  def escolha_palavra_secreta
-   puts "Escolhendo uma plavrava secreta..."
-   palavra_secreta = "programador"
-   puts "Palavra secreta com #{palavra_secreta} letras... boa sorte!"
-   palavra_secreta
-  end
-
-  def nao_quer_jogar?
-    puts "Deseja jogar novamente? (S/N)"
-    quero_jogar = gets.strip
-    nao_quer_jogar = quero_jogar.upcase == "N"
-  end
-
-  def pede_um_chute(chutes, erros)
-    puts "\n\n"
-    puts "Erros ate agora: #{erros}"
-    puts "Chutes ate agora: #{chutes}"
-    puts "Entre com uma letra ou uma palavra"
-    chute = gets.strip
-    puts "Será que você acertou? Você chutou #{chute}"
-    chute
-  end
+ require_relative 'ui_forca'
 
   def joga(nome)
     palavra_secreta = escolha_palavra_secreta
@@ -39,7 +10,7 @@
     while erros < 5
       chute = pede_um_chute chutes, erros
       if chutes.include? chute
-        puts "Você já chutou #{chute}"
+        avisa_chute_efetuado chute
         next
       end
       chutes << chute
@@ -57,20 +28,20 @@
         end
 
         if total_encontrado == 0
-          puts "Letra não encontrada."
+          avisa_letra_nao_encontrada
           erros += 1
         else
-          puts "Letra encontrada #{total_encontrado} vezes."
+          avisa_letra_encontrada total_encontrado
         end
 
       else
         acertou = chute == palavra_secreta
         if acertou
-          puts "Parabéns! Acertou"
+          avisa_acertou_a_palavra
           pontos_ate_agora += 100
           break
         else
-          puts "Que pena... errou"
+          avisa_errou_a_palavra
           pontos_ate_agora -= 30
           erros += 1
         end
@@ -78,7 +49,7 @@
 
     end
 
-    puts "Você ganhou #{pontos_ate_agora} pontos."
+    avisa_pontos pontos_ate_agora
 
   end
 
