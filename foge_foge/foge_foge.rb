1159bcee8bf369ced837896a0ec1867f3dfaf10f
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-24 02:09:55 -0200

    Extracting the position

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -8,24 +8,35 @@ require_relative 'ui'
 
 def encontra_jogador(mapa)
     caracter_do_heroi = "H"
-    for linha = 0..(mapa.size-1)
-        linha_atual = mapa[linha]
+    mapa.each_with_index do |linha_atual, linha|
         coluna_do_heroi = linha_atual.index caracter_do_heroi
         if coluna_do_heroi
-            # achei!
+            return [linha, coluna_do_heroi]
         end
     end
     # não achei!
 end
 
-  def joga(nome)
+def joga(nome)
     mapa = le_mapa(1)
-
     while true
-      desenha mapa
-      direcao = pede_movimento
+        desenha mapa
+        direcao = pede_movimento
+        heroi = encontra_jogador mapa
+        mapa[heroi[0]][heroi[1]] = " "
+        case direcao
+            when "W"
+                heroi[0] -= 1
+            when "S"
+                heroi[0] += 1
+            when "A"
+                heroi[1] -= 1
+            when "D"
+                heroi[1] += 1
+        end
+        mapa[heroi[0]][heroi[1]] = "H"
     end
-  end
+end
 
   def inicia_fogefoge
     nome = da_boas_vindas
