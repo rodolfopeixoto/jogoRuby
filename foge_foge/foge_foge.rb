c9d8da4df1c6318a842e72bdf75707b231b40580
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-24 02:16:27 -0200

    Created movement

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -18,6 +18,7 @@ def encontra_jogador(mapa)
 end
 
 def calcula_nova_posicao(heroi, direcao)
+    heroi = heroi.dup
     case direcao
         when "W"
             heroi[0] -= 1
@@ -31,14 +32,39 @@ def calcula_nova_posicao(heroi, direcao)
     heroi
 end
 
+def posicao_valida?(mapa, posicao)
+    linhas = mapa.size
+    colunas = mapa[0].size
+
+    estourou_linha = posicao[0] < 0 || posicao[0] >= linhas
+    estourou_coluna = posicao[1] < 0 || posicao[1] >= colunas
+
+    if estourou_linha || estourou_coluna
+        return false
+    end
+
+    if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
+        return false
+    end
+
+    true
+end
+
+
+
 def joga(nome)
     mapa = le_mapa(1)
     while true
         desenha mapa
         direcao = pede_movimento
+
         heroi = encontra_jogador mapa
-        mapa[heroi[0]][heroi[1]] = " "
         nova_posicao = calcula_nova_posicao heroi, direcao
+        if !posicao_valida? mapa, nova_posicao
+            next
+        end
+
+        mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
     end
 end
