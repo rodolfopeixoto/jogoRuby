d609672ce92ba29d1c32161ea497fba1fbe1da73
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-24 23:00:58 -0200

    Created ghost to move method

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -19,16 +19,15 @@ end
 
 def calcula_nova_posicao(heroi, direcao)
     heroi = heroi.dup
-    case direcao
-        when "W"
-            heroi[0] -= 1
-        when "S"
-            heroi[0] += 1
-        when "A"
-            heroi[1] -= 1
-        when "D"
-            heroi[1] += 1
-    end
+    movimentos = {
+        "W" => [-1, 0],
+        "S" => [+1, 0],
+        "A" => [0, -1],
+        "D" => [0, +1]
+        }
+    movimento = movimentos[direcao]
+    heroi[0] += movimento[0]
+    heroi[1] += movimento[1]
     heroi
 end
 
@@ -42,21 +41,42 @@ def posicao_valida?(mapa, posicao)
     if estourou_linha || estourou_coluna
         return false
     end
-
-    if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
+    valor_atual = mapa[posicao[0]][posicao[1]]
+    if valor_atual == "X" || valor_atual == "F"
         return false
     end
 
     true
 end
 
+def move_fantasma(mapa,linha,coluna)
+  posicao = [linha, coluna + 1]
+  if posicao_valida? mapa, posicao
+    mapa[linha][coluna] = " "
+    mapa[posicao[0]][posicao[1]] = "F"
+  end
+
+end
+
+def move_fantasmas(mapa)
+  caractere_do_fantasma = "F"
+  mapa.each_with_index do | linha_atual, linha |
+    linha_atual.chars.each_with_index do |caracteres_atual, coluna|
+      eh_fantasma = caracteres_atual == caractere_do_fantasma
+      if eh_fantasma
+        move_fantasma mapa, linha, coluna
+      end
+    end
+  end
+end
+
 
 
 def joga(nome)
-    mapa = le_mapa(1)
+    mapa = le_mapa(2)
     while true
         desenha mapa
-        direcao = pede_movimento
+        direcao = pede_movimento #W
 
         heroi = encontra_jogador mapa
         nova_posicao = calcula_nova_posicao heroi, direcao
@@ -66,6 +86,8 @@ def joga(nome)
 
         mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+
+        move_fantasmas mapa
     end
 end
 
