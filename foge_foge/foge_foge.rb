8ddbafeaca20c3fe325c8bb9b5d4d065fd390693
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-25 00:31:54 -0200

    Created new map

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -49,59 +49,56 @@ def posicao_valida?(mapa, posicao)
     true
 end
 
-def posicoes_validas_a_partir(mapa, posicao)
+def posicoes_validas_a_partir(mapa, novo_mapa, posicao)
   posicoes  = []
   baixo     = [posicao[0] + 1, posicao[1]]
-  if posicao_valida? mapa, baixo
+  if posicao_valida?(mapa, baixo) && posicao_valida?(novo_mapa, baixo)
     posicoes << baixo
   end
   cima      = [posicao[0], posicao[1] + 1]
-  if posicao_valida? mapa, cima
+  if posicao_valida?(mapa, cima) && posicao_valida?(novo_mapa, cima)
     posicoes << cima
   end
   direita   = [posicao[0], posicao[1] + 1]]
-  if posicao_valida? mapa, direita
+  if posicao_valida?(mapa, direita) && posicao_valida?(novo_mapa, direita)
     posicoes << direita
   end
   esquerda  = [posicao[0], posicao[1] - 1]
-  if posicao_valida? mapa, esquerda
+  if posicao_valida?(mapa, esquerda) && posicao_valida?(novo_mapa, esquerda)
     posicoes << esquerda
   end
   posicoes
 end
 
   def copia_mapa(mapa)
-    novo_mapa = []
-    mapa.each do |linha|
-      nova_linha = linha.dup.tr "F", " "
-      novo_mapa << nova_linha
-    end
-    novo_mapa
+    novo_mapa = map.join("\n").tr("F"," ").split("\n")
   end
 
-def move_fantasma(mapa,linha,coluna)
-  posicoes = posicoes_validas_a_partir mapa, [linha,coluna]
+def move_fantasma(mapa, novo_mapa,linha,coluna)
+  posicoes = posicoes_validas_a_partir mapa, novo_mapa, [linha,coluna]
 
   return if posicoes.empty?
 
   posicao = posicoes[0]
   if posicao_valida? mapa, posicao
     mapa[linha][coluna] = " "
-    mapa[posicao[0]][posicao[1]] = "F"
+    novo_mapa[posicao[0]][posicao[1]] = "F"
   end
 
 end
 
 def move_fantasmas(mapa)
   caractere_do_fantasma = "F"
+  novo_mapa = copia_mapa mapa
   mapa.each_with_index do | linha_atual, linha |
     linha_atual.chars.each_with_index do |caracteres_atual, coluna|
       eh_fantasma = caracteres_atual == caractere_do_fantasma
       if eh_fantasma
-        move_fantasma mapa, linha, coluna
+        move_fantasma mapa, novo_mapa, linha, coluna
       end
     end
   end
+  novo_mapa
 end
 
 
@@ -121,7 +118,7 @@ def joga(nome)
         mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
 
-        move_fantasmas mapa
+        mapa = move_fantasmas mapa
     end
 end
 
