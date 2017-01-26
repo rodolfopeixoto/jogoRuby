7c274d8ae2afde59726d7f6ed6704845ebcfbf81
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-26 00:09:50 -0200

    Refactoring estoque class

--- ooruby/livro.rb
+++ ooruby/livro.rb
@@ -1,13 +1,16 @@
 class Livro
   attr_reader :titulo, :preco, :ano_lancamento, :editora, :tipo
 
-  def initialize(titulo, preco, ano_lancamento,possui_reimpressao, editora, tipo)
+  def initialize(titulo, preco, ano_lancamento,possui_reimpressao, editora, tipo, possui_sobrecapa, numero)
     @titulo             = titulo
     @ano_lancamento     = ano_lancamento
     @preco              = calcula_preco(preco)
     @possui_reimpressao = possui_reimpressao
     @editora            = editora
     @tipo               = tipo
+    @possui_sobrecapa   = possui_sobrecapa
+    @numero             = numero
+
   end
 
   def calcula_preco(base)
